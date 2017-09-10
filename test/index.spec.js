2ac3aef3c38ba95cb3e793e5bf09fbc5f2845442
Jon Lauridsen <mail@jonlauridsen.com>
2017-09-10 13:55:33 +0100

    Test pattern-must-be-string logic

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,5 @@
 /* global describe, it */
+const assert = require('assert')
 const nunjucks = require('../lib/index')
 
 describe('metalsmith-nunjucks', function () {
@@ -17,4 +18,24 @@ describe('metalsmith-nunjucks', function () {
   it('calls extensions on initialisation', function (done) {
     nunjucks({}, () => done())
   })
+
+  it('fails to run if pattern is an array', function (done) {
+    assertRun({pattern: 'foo'}, function (err) {
+      assert.equal(err, undefined)
+      done()
+    })
+  })
+
+  it('fails to run if pattern is an array', function (done) {
+    assertRun({pattern: ['foo']}, function (err) {
+      assert(err instanceof Error)
+      done()
+    })
+  })
+
 })
+
+const assertRun = function (opts, callback) {
+  const instance = nunjucks(opts)
+  instance({}, {}, callback)
+}
